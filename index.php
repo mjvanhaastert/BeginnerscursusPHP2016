22f9e89208550cd4494c1582289ad621d358a968
Mark Jeff van Haastert <mjvanhaastert@hotmail.com>
2018-04-09 09:23:03 +0200

    070 php variabelen weergeven

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<html>
-<head>
-    <title>PHP Test</title>
-</head>
-<body>
-<?php echo '<p>Hello World</p>'; ?>
-</body>
-</html>
+<?php
+
+$aantal = 14;
+$dezejongen = "deze jongen is ";
+echo $dezejongen.$aantal." jaar uit!";
